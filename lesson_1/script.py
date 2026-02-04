688fc7d654d3f295f9676c8a9c5ffee542fa5e9c
Ладыгин Никита <olesya_lakunina@MacBook-Air-de-Olesa.local>
2026-02-04 18:41:16 +0300

    fixed errors

--- lesson_1/script.py
+++ lesson_1/script.py
@@ -2,14 +2,19 @@ import requests
 
 
 URL = 'https://wttr.in/'
-payload = 'nTqM&lang=ru'
-cities = ['london', 'svo', 'cherepovets']
+PAYLOAD = {'nTqM': '', 'lang': 'ru'}
+CITIES = ['london', 'svo', 'cherepovets']
 
-if __name__ == '__main__':
-    try:
-        for city in cities:
-            response = requests.get(f'{URL}{city}', params=payload)
+def main():
+    for city in CITIES:
+        try:
+            response = requests.get(f'{URL}{city}', params=PAYLOAD)
             response.raise_for_status()
             print(response.text)
-    except requests.exceptions.HTTPError as error:
-        exit(f'Ошибка получения данных с сервера:\n{error}')
+        except requests.exceptions.HTTPError as error:
+            print(f'Ошибка получения данных с сервера:\n{error}')
+        except Exception as error:
+            print(f'Неизвестная ошибка:\n{error}')
+
+if __name__ == '__main__':
+    main()
